1e8301150678066d87f4a37164ca216863f8e17a
jmangas <jmangas@uoc.edu>
2015-02-04 13:33:06 +0100

    minor changes

--- rasp.js
+++ rasp.js
@@ -1,7 +1,19 @@
+var colors = require('colors');
 var serlcd = require('./serlcd.js');
-var lcd = new serlcd("/dev/ttyAMA0");
-console.log('lcd configured');
+var dev = "/dev/tty1"; 
+console.log('using : ' + dev.blue);
+var lcd = new serlcd(dev);
+console.log('lcd configured'.green);
+
+//console.log(process.env);
+
 
 lcd.clearScreen();
 lcd.write("Hooray!");
-console.log('bye...');
+
+console.log('bye...'.green);
+
+process.on('exit', function(code) {
+  //console.log('About to exit with code:'.red, code);
+});
+process.exit();
